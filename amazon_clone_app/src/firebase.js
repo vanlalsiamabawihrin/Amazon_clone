ed833bf767630a000905abbc7f5c5a09f9457c00
siama2017 <siama2017@users.noreply.github.com>
2020-09-16 20:49:50 +1000

    initial commit

---
+++ amazon_clone_app/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAggQsLxHIddAzcPRMOlTcvQw8RrfBZ18E",
+    authDomain: "clone-app-9b442.firebaseapp.com",
+    databaseURL: "https://clone-app-9b442.firebaseio.com",
+    projectId: "clone-app-9b442",
+    storageBucket: "clone-app-9b442.appspot.com",
+    messagingSenderId: "859473888799",
+    appId: "1:859473888799:web:206c811f7f8084c2b0772a",
+    measurementId: "G-XHZR5KVSPR"
+  };
