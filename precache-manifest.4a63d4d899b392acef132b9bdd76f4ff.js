50b3d2f978a4105725598fd52817c844d91cce1a
siama2017 <siama2017@users.noreply.github.com>
2020-11-23 14:59:34 +1100

    Updates

---
+++ precache-manifest.4a63d4d899b392acef132b9bdd76f4ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc8a5e95ec485762a52a3446de7bbb68",
+    "url": "/github.io/amazon_clone/index.html"
+  },
+  {
+    "revision": "f6932c43369e6ca97091",
+    "url": "/github.io/amazon_clone/static/css/main.2be35fa7.chunk.css"
+  },
+  {
+    "revision": "04d501b5edcb28070782",
+    "url": "/github.io/amazon_clone/static/js/2.246c0946.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/github.io/amazon_clone/static/js/2.246c0946.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6932c43369e6ca97091",
+    "url": "/github.io/amazon_clone/static/js/main.d15e7a18.chunk.js"
+  },
+  {
+    "revision": "1f2614ee5199b166c72c",
+    "url": "/github.io/amazon_clone/static/js/runtime-main.b9c6c483.js"
+  }
+]);
